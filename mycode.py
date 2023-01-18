46e21af3ed790a450c472a10c7a9a05c2942014b
Meryem Yuksel <myuksel@ufl.edu>
2023-01-18 12:24:28 -0500

    plotting the yearly publication figure, only thing needed is titles for it now

--- mycode.py
+++ mycode.py
@@ -1,13 +1,21 @@
 import pandas
+import matplotlib.pyplot as plot
+#Problem 1
 
 #read the files
-authors = pandas.read_csv("authorinfo.csv")
-articles = pandas.read_csv("articleinfo.csv")
+authors = pandas.read_csv("authorInfo.csv")
+articles = pandas.read_csv("articleInfo.csv")
 
 
-#now, I will merge them
+#now, I will merge them into one data frame
 merged = authors.merge(articles, on = "Article No.")
+
+#filling the empty cells with the value 0
+merged.fillna(0)
 
 #check if they merged well by printing it out in a file.
 merged.to_csv("merged.csv", index = False)
 
+#1.1 yearly_publication figure
+pandas.value_counts(merged['Year']).plot.bar()
+plot.show()
