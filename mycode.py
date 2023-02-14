2e1851740872f9792aeca2c7bacab0aad9d6bb9a
Meryem Yuksel <myuksel@ufl.edu>
2023-02-14 14:57:02 -0500

    did the classification model evaluations and now just need to explain how they work

--- mycode.py
+++ mycode.py
@@ -5,6 +5,14 @@ import country_converter as toCode
 import numpy as np
 import statsmodels.api as sm
 from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix, roc_curve, auc, classification_report
 import seaborn as sns
 #Problem 1
 
@@ -76,7 +84,8 @@ data['Gender'] = encoder.transform(data['Gender'])
 
 #putting on a histogram plot the satisfaction survey results
 sns.histplot(data=data['SUS'], palette='bright')
-plot.show()
+plot.title("Histogram of System Usability Survey Scores")
+#plot.show()
 
 print()
 print("The correlation coefficients between the independent variables and the dependent variable")
@@ -91,3 +100,84 @@ x = sm.add_constant(x)
 
 rmodel = sm.OLS(y, x).fit()
 print(rmodel.summary());
+
+#Question 3:
+#Train a Classification model in which: the independent variables
+#(inputs) are “ASR_Error,” “IntentError,” “Duration,” and “Gender,”; the dependent
+#variable (output) is “Purchase.” Use the evaluation metrics we introduced in class to
+#compare the performance of the following four machine learning classification
+#algorithms: (1) Logistic Regression, (2) SVM, (3) Naive Bayes, and (4) Random
+#Forest.
+
+# We will be using the same data as last time, but since the na values will be filled
+#with the mean value this time, we are re-reading
+data = pandas.read_csv('data.csv')
+#print(data.isna().sum())
+#only unnamed has na values, which we will not use
+
+#now preparing data
+y = data['Purchase'].to_numpy()
+x = data[['ASR_Error', 'Intent_Error', 'Duration', 'Gender']].to_numpy()
+
+#scaling the data
+scaler = StandardScaler()
+scaled_x = scaler.fit_transform(x)
+
+x_train, x_test, y_train, y_test = train_test_split(scaled_x, y, test_size = 0.3)
+
+logistic = LogisticRegression()
+svc = SVC(probability=True)
+naiveB = GaussianNB()
+randomForest = RandomForestClassifier()
+
+#fitting the data into the model
+logistic.fit(x_train,y_train)
+svc.fit(x_train,y_train)
+naiveB.fit(x_train,y_train)
+randomForest.fit(x_train,y_train)
+
+#now get your predictions
+logisticY = logistic.predict(x_test)
+logisticY_pr = logistic.predict_proba(x_test)
+
+svcY = svc.predict(x_test)
+svcY_pr = svc.predict_proba(x_test)
+
+naiveY = naiveB.predict(x_test)
+naiveY_pr = naiveB.predict_proba(x_test)
+
+rfcY = randomForest.predict(x_test)
+rfcY_pr = randomForest.predict_proba(x_test)
+
+#evaluate with confusion matrix, roc curve, and auc
+print()
+models = ['Logistic Regression', 'Support Vector Machine', 'Naive Bayes Classifier', 'Random Forest Classifier']
+predicted = [logisticY, svcY, naiveY, rfcY]
+probabilities = [logisticY_pr, svcY_pr, naiveY_pr, rfcY_pr]
+
+
+
+for  predictedvalue, model in zip(predicted, models):
+    disp = ConfusionMatrixDisplay(confusion_matrix(y_test.ravel(),predictedvalue))
+    disp.plot(
+        include_values=True,
+        cmap='gray',
+        colorbar=False
+    )
+    disp.ax_.set_title(f"{model} Confusion Matrix")
+
+plot.show()
+plot.figure(figsize=(30, 15))
+plot.suptitle("ROC Curves")
+plot_index = 1
+for model, probability in zip(models,probabilities):
+    fpRate, tpRate, thresholds = roc_curve(y_test, probability[:, 1])
+    areaUnderCurve = auc(fpRate, tpRate)
+    plot.subplot(3, 2, plot_index)
+    plot.plot(fpRate, tpRate, 'r', label='ROC curve')
+    plot.title(f'ROC Curve For Model: {model} - [AUC - {areaUnderCurve}]', fontsize=12)
+    plot.xlabel('FP Rate', fontsize=11)
+    plot.ylabel('TP Rate', fontsize=11)
+    plot.legend()
+    plot_index += 1
+plot.show()
